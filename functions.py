f1d1cd7baa6b1e1c0f762fb58cb3bcea9e541bdf
Sofiane Haddad <sofiane>
2013-02-20 11:58:00 +0100

    Added some functions to get list of all factories. In progress

---
+++ functions.py
@@ -0,0 +1,77 @@
+import openturns as ot
+
+def getOTVersion():
+    ''' Get the openturns version for PlatformInfo as a list of three integer : (major, minor, revision) '''
+    openturnsVersion = ot.PlatformInfo.GetVersion()
+    # check if current software version is a devel one!
+    if (openturnsVersion.count('based-devel') > 0):
+        index = openturnsVersion.index('based-devel') - 1 # (-1) because we remove -
+        ot.Log.Warn('You are using a based-devel vesion')
+        openturnsVersion = openturnsVersion[0:index]
+    # The ot version should be numeric here, separated with '.'
+    openturnsVersion = openturnsVersion.split('.')
+    # Parsing and getting all elements of version
+    try:
+        major = openturnsVersion[0]
+        majorInt = int(major)
+    except:
+        raise ValueError('Incorrect major number')
+    try:
+        minor = openturnsVersion[1]
+        minorInt = int(minor)
+    except:
+        raise ValueError('Incorrect minor number')
+
+    try:
+        revision = openturnsVersion[2]
+    except:
+        UserWarning('No revision version found')
+        revisionInt = 0
+    return [majorInt, minorInt, revisionInt]
+
+def ListAllFactoryName():
+    """ Return the name of all the DistributionFactory in OT
+        Care! May be incorrect with too older version because of SWIG modules were
+        differents (no dist maybe?)
+    """
+    return(AllFactoryName)
+
+
+
+def ListAllFactory():
+    """ Return a dictionnary with DistributionFactory objects of OT with the distinction
+        'AllContinuousFactory' and 'AllDiscreteFactory'
+        Care! May be incorrect with too older version because of SWIG modules were
+        differents (no dist maybe?)
+    """
+    allDist = dir(ot.dist)
+    result = {}
+    AllContinuousFactoryName = []
+    AllContinuousFactory =[]
+    AllDiscreteFactoryName = []
+    AllDiscreteFactory =[]
+    AllFactoryName = []
+    for elt in allDist:
+        if ('Factory' in elt) and ('_' not in elt) and ('Histogram' not in elt):
+            AllFactoryName.append(elt)
+	    factoryName = 'ot.' + elt
+	    StrDist = factoryName.replace('Factory','()')
+	    Dist = eval(StrDist)
+	    if Dist.isContinuous():
+		factoryName = factoryName.replace('Factory','Factory()')
+		AllContinuousFactoryName.append(factoryName)
+		factory = eval(factoryName)
+		AllContinuousFactory.append(factory)
+	    else :
+		factoryName = factoryName.replace('Factory','Factory()')
+		AllDiscreteFactoryName.append(factoryName)
+		factory = eval(factoryName)
+		AllDiscreteFactory.append(factory)
+    result['AllContinuousFactoryName'] = AllContinuousFactoryName
+    result['AllContinuousFactory'] = AllContinuousFactory
+    result['AllDiscreteFactoryName'] = AllDiscreteFactoryName
+    result['AllDiscreteFactory'] = AllDiscreteFactory
+    return(result)
+
+# Check if function contains catalog or if it should make it here self!
+#def hasCatalog(major, minor, revision):
