1aa4c50dc71b3f057c50ee2fa0d02164985cc21c
Sofiane HADDAD <sofiane_haddad@yahoo.fr>
2013-04-11 14:25:31 +0200

    [src] Cosmetics : updated head's docstring and some missing comments

--- src/MultivariateRandomMixture.py
+++ src/MultivariateRandomMixture.py
@@ -43,12 +43,16 @@ mvrm_resource_map = {"MultivariateRandomMixture-DefaultAlpha" : 4,\
 class PythonMultivariateRandomMixture(ot.PythonDistribution):
     """
     Multivariate distribution
-    Modelization of a multivariate random mixture distribution :
+    Modelization of a multivariate random mixture distribution of form:
     Y = y_0 + M X, Y of size d
-    where M is a dxn matrix, X is a n-random vector with independent components
+    where M is a d x n matrix, X is a n-random vector with independent components and y_0 a deterministic vector.
+    This is a generalization of the RandomMixture distribution.
 
+    Its moments either some realizations are easy to get. The characteristic function could be deduced easily using the
+    independance of X's marginals.
     The probability density function (p.d.f) is computed using the Poisson summation formula :
-    \sum_{j_1 \in \mathbb{Z}}\ldots \sum_{j_d \in \mathbb{Z}} p() = ...
+    \sum_{j_1 \in \mathbb{Z}}... \sum_{j_d \in \mathbb{Z}} p(y_1 + \frac{2\pi j_1}{h_1},...,y_d + \frac{2\pi j_d}{h_d}) =
+    \frac{h_1 x... x h_d}{2^d \pi^d} \sum_{k_1 \in \mathbb{Z}}... \sum_{k_d \in \mathbb{Z}} \phi(k_1 h_1,...,k_d h_d) * exp(-i(k_1 h_1+...+k_d h_d))
 
     """
 
@@ -67,10 +71,8 @@ class PythonMultivariateRandomMixture(ot.PythonDistribution):
         -------
         >>> import openturns as ot
         >>> import MultivariateRandomMixture as MV
-        >>> dist = MV.PythonMultivariateRandomMixture.PythonMultivariateRandomMixture(\
-                  ot.DistributionCollection([ot.Normal(), ot.Uniform()]), \
-                  ot.Matrix([[1,1], [3,4], [2, -1]]), \
-                  [0, 5])
+        >>> dist = MV.PythonMultivariateRandomMixture(ot.DistributionCollection([ot.Normal(), ot.Uniform()]), \
+                  ot.Matrix([[1,1], [3,4], [2, -1]]), [0, 5])
 
         """
         n = len(collection)
@@ -79,6 +81,10 @@ class PythonMultivariateRandomMixture(ot.PythonDistribution):
         # Use of setDistributionCollection method
         self.setDistributionCollection(collection)
         # matrix cast
+        # As object might be a numpy class, we convert into
+        # ot type before checking
+        # Also, if the object is of type SquareMatrix,
+        # the getNbColumns/getNbRows methods are not available
         self.matrix = ot.Matrix(matrix)
         # Check matrix dimension
         if(self.matrix.getNbColumns() != n):
@@ -87,15 +93,17 @@ class PythonMultivariateRandomMixture(ot.PythonDistribution):
         if (d > 3):
             raise ValueError("Mixture should be of dimension 1, 2 or 3")
         if (y0 is None):
-            self.y0 = ot.NumericalPoint(d * [0])
+            self.y0 = ot.NumericalPoint(d * [0.0])
         else :
             assert len(y0) == d
             self.y0 = ot.NumericalPoint(y0)
+        # Set the distribution dimension
         ot.PythonDistribution.__init__(self, d)
-        # Set alpha and beta values
+        # Set alpha and beta values from the resource map
         self.alpha = mvrm_resource_map['MultivariateRandomMixture-DefaultAlpha']
         self.beta = mvrm_resource_map['MultivariateRandomMixture-DefaultBeta']
         # compute the mean and covariance
+        # as mean is easy, no need to use isComputedMean attributs
         self.computeMean()
         self.computeCovariance()
         # set the standard deviation
