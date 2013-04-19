705a4e6a807d49a7f145a2c8b6102dcd9d31bac7
Sofiane HADDAD <sofiane_haddad@yahoo.fr>
2013-04-19 14:04:58 +0200

    [test] added a test for profiling computePDF method

---
+++ test/test_profiling.py
@@ -0,0 +1,81 @@
+
+
+import openturns as ot
+import MultivariateRandomMixture as MV
+import numpy as np
+import time
+
+N = 100
+maxSize = 0
+blockMin = 0
+blockMax = 1
+
+
+#ot.Log.Show(ot.Log.ALL)
+
+collection = ot.DistributionCollection(100 * [ot.Uniform()])
+matrix = ot.Matrix([100 * [1.0]])
+distribution = MV.PythonMultivariateRandomMixture(collection, matrix)
+
+
+interval = distribution.getRange()
+mean = distribution.getMean()
+cov = distribution.getCovariance()
+sigma = distribution.getStandardDeviation()
+sample = distribution.getSample(N)
+print "MultivariateRandomMixture distribution"
+print "range = ", interval
+print "mean = ", mean
+print "cov = ", cov
+print "sigma = ", sigma
+# Equivalent random mixture
+random_mixture = ot.RandomMixture(collection)
+# same attributs
+random_mixture.setAlpha(distribution.getAlpha())
+random_mixture.setBeta(distribution.getBeta())
+random_mixture.setReferenceBandwidth(distribution.getReferenceBandwidth()[0])
+
+distribution.setMaxSize(maxSize)
+distribution.setBlockMin(blockMin)
+distribution.setBlockMax(blockMax)
+
+random_mixture.setMaxSize(maxSize)
+random_mixture.setBlockMin(blockMin)
+random_mixture.setBlockMax(blockMax)
+
+print "RandomMixture distribution"
+print "range = ", random_mixture.getRange()
+print "mean = ", random_mixture.getMean()
+print "cov = ", random_mixture.getCovariance()
+print "sigma = ", random_mixture.getStandardDeviation()
+print "sample :"
+print "min = %s\nmax = %s\nmean = %s, cov = %s" %(sample.getMin(),sample.getMax(), sample.computeMean(), sample.computeCovariance())
+# evaluation of the characteristic function between xmin and xmax
+xmin, xmax, dx = -20.0, 20.0, 1.0
+x = np.arange(xmin, xmax + dx, dx)
+delta = 0.0
+pdf = 0.0
+t1 = []
+t2 = []
+for value in x:
+    tic = time.time()
+    c1 = distribution.computePDF([value])
+    toc = time.time()
+    dt1 = toc - tic
+    t1.append(dt1)
+    tic = time.time()
+    c2 = random_mixture.computePDF(value)
+    toc = time.time()
+    dt2 = toc - tic
+    t2.append(dt2)
+    print dt1, dt2, dt1/dt2
+    print distribution.pdfEpsilon_,  random_mixture.getPDFEpsilon()
+    print c1, c2, (c1 - c2)/c2
+
+size = len(t1)
+import matplotlib.pylab as plt
+plt.loglog(x, t1, 'r', label = 'MV')
+plt.loglog(x, t2, 'b', label = 'RM')
+plt.legend()
+plt.ion()
+plt.show()
