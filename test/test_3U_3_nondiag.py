3bc0af9d49e46d2da58f943f461bc22889ee496b
Sofiane HADDAD <sofiane_haddad@yahoo.fr>
2013-05-15 19:31:41 +0200

    [test] updated test_3U_3_nondiag : added FFT computations

--- test/test_3U_3_nondiag.py
+++ test/test_3U_3_nondiag.py
@@ -34,6 +34,13 @@ if __name__ == "__main__":
     import openturns as ot
     import MultivariateRandomMixture as MV
     import numpy as np
+    blockMin = 3
+    blockMax = 6
+    n_blockMax = 2**blockMax
+    n = 4 * n_blockMax * (n_blockMax + 1) * (2 * n_blockMax + 1) + 2 * n_blockMax
+    maxSize = n
+    b = 6.0
+    N = 128
 
     """
     Test
@@ -42,6 +49,9 @@ if __name__ == "__main__":
     collection = ot.DistributionCollection([ot.Uniform(), ot.Uniform(), ot.Uniform()])
     matrix = ot.Matrix([[1, 4, 5], [3, 2, -1], [0,0,1]])
     distribution = MV.PythonMultivariateRandomMixture(collection, matrix)
+    distribution.setBlockMin(blockMin)
+    distribution.setBlockMax(blockMax)
+    distribution.setGridMesher(MV.MaxNormMeshGrid.CachedMeshGrid(MV.MaxNormMeshGrid.SkinCube3D(distribution.getReferenceBandwidth(), symmetric=True), size=maxSize))
     interval = distribution.getRange()
     mean = distribution.getMean()
     cov = distribution.getCovariance()
@@ -58,9 +68,9 @@ if __name__ == "__main__":
     dY = ymax - ymin
     dZ = zmax - zmin
     # 3D grid
-    dx = 0.5
-    dy = 0.5
-    dz = 0.5
+    dx = 1.0
+    dy = 1.0
+    dz = 1.0
     x = slice(xmin + 0.2 * dX, xmax - 0.2 * dX, dx)
     y = slice(ymin + 0.2 * dY, ymax - 0.2 * dY, dy)
     z = slice(zmin + 0.2 * dZ, zmax - 0.2 * dZ, dz)
@@ -69,13 +79,16 @@ if __name__ == "__main__":
     values_th = np.ndarray(shape)
     values_app = np.ndarray(shape)
     # Compute inverse of transformation matrix
+    print "Analytic estimate"
     for ix in range(shape[0]):
         for iy in range(shape[1]):
             for iz in range(shape[2]):
                 u = [grid_x[ix, iy, iz], grid_y[ix, iy, iz], grid_z[ix, iy, iz]]
                 pdf_estimate = distribution.computePDF(u)
-                print "point=%s, pdf_value=%s"%(u, pdf_estimate)
                 values_th[ix, iy, iz]=  pdf_estimate
+    # computation on a regular grid
+    [regular_grid, values_th_grid]= distribution.computePDFOn3DGrid(b, N)
+    print "Analytic estimate done"
     # For the validation of the poisson formula, we compute again the pdf
     print "Non analytic estimate"
     distribution.isAnalyticPDF_ = False
@@ -84,5 +97,9 @@ if __name__ == "__main__":
             for iz in range(shape[2]):
                 u = [grid_x[ix, iy, iz], grid_y[ix, iy, iz], grid_z[ix, iy, iz]]
                 pdf_estimate = distribution.computePDF(u)
-                values_app[ix, iy, iz]=  pdf_estimate
-                print "point=%s, pdf_value=%s"%(u, pdf_estimate)
+                values_app[ix, iy, iz] =  pdf_estimate
+                pdf_theo = values_th[ix, iy, iz]
+                print "point=%s, pdf_theoritical=%s, pdf_estimate=%s"%(u, pdf_theo, pdf_estimate)
+    # computation on a regular grid
+    [regular_grid, values_app_grid]= distribution.computePDFOn3DGrid(b, N)
+    print "Non analytic estimate done"
